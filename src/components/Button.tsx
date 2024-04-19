cf74bfb32225d497e3d134971a463888cbc9fe45
lmReef <ReefMatson@gmail.com>
2024-04-19 22:06:13 +0700

    feat: implement NavBar, rename css to scss
    - start on creating template

---
+++ src/components/Button.tsx
@@ -0,0 +1,30 @@
+import "./Button.scss";
+
+interface ButtonProps {
+  text?: string;
+  image?: string;
+  href?: string;
+  newTab?: boolean;
+  children?: React.ReactElement;
+}
+
+const Button: React.ElementType = ({
+  text,
+  image,
+  href,
+  newTab,
+  children,
+}: ButtonProps) => {
+  console.log(newTab ? "_blank" : "_self");
+  return (
+    <a href={href} target={newTab ? "_blank" : "_self"}>
+      <div className="button">
+        {/* display button content */}
+        {text ? text : image ? <img src={image} /> : ""}
+        {children}
+      </div>
+    </a>
+  );
+};
+
+export default Button;
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,36 @@
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { ElementType } from "react";
+import logo from "../logo.svg";
+import Button from "./Button";
+import "./NavBar.scss";
+
+const NavBar: ElementType = () => {
+  return (
+    <nav id="navbar">
+      <div className="nav-block left">
+        <a href="/">
+          <div className="logo">
+            <img src={logo} />
+            <h1>Basic Template</h1>
+          </div>
+        </a>
+      </div>
+
+      <div className="nav-block center">
+        <Button text="More Templates" href="/" newTab>
+          <FontAwesomeIcon className="icon" icon={faArrowUpRightFromSquare} />
+        </Button>
+      </div>
+
+      <div className="nav-block right">
+        <Button href="https://github.com/lmReef/website-template-basic" newTab>
+          <FontAwesomeIcon className="icon" icon={faGithub} />
+        </Button>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
